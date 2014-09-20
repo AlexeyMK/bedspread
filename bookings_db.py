12d1fc84570142fce08231c592a7471a90398e23
Alexey Komissarouk <alexey@alexeymk.com>
2014-09-20 13:38:19 -0600

    refactored gspread interface into bookings_db.py

---
+++ bookings_db.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+import gspread
+
+
+DATE_FORMAT = "%Y-%m-%d"
+
+
+def daterange(start_date, end_date):
+  for n in range(int((end_date - start_date).days)):
+    yield start_date + timedelta(n)
+
+
+class BookingsDB(object):
+  def __init__(self):
+    # { "double 2": [booking, booking, booking...]}
+    self.bookings_by_room = defaultdict(list)
+    # { "double 2": [12/01/2014, 13/01/2014...]} (datetime objects)
+    self.dates_occupied_by_room = defaultdict(set)
+
+    self.gc = gspread.login('hackerparadise2014@gmail.com', 'hacker2014')
+    self._load_bookings()
+
+  def _load_bookings(self):
+    # Open a worksheet from spreadsheet with one shot
+    spreadsheet = self.gc.open("Bookings")
+    bookings = spreadsheet.worksheet("Bookings")
+
+    for room_id, name, checkin_date, checkout_date in bookings.get_all_values()[1:]:
+      self.bookings_by_room[room_id].append({
+        "name": name,
+        "checkin_date": datetime.strptime(checkin_date, DATE_FORMAT),
+        "checkout_date": datetime.strptime(checkout_date, DATE_FORMAT),
+        "room_id": room_id
+      })
+
+    for room_id, bookings in self.bookings_by_room.iteritems():
+      for booking in bookings:
+        # TODO(AMK) error handling for double-booking
+        for booked_date in daterange(booking["checkin_date"],
+                                     booking["checkout_date"]):
+          self.dates_occupied_by_room[room_id].add(booked_date)
+
+  def room_available_during_range(self, room, start_date, end_date):
+    return all(date not in self.dates_occupied_by_room[room]
+               for date in daterange(start_date, end_date))
--- web.py
+++ web.py
@@ -1,48 +1,12 @@
 from flask import Flask, render_template, request
-from collections import defaultdict
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.parser import parse
 
-import gspread
+from bookings_db import daterange, BookingsDB
 
 
 app = Flask(__name__)
-print "loading spreadsheet..."
-gc = gspread.login('hackerparadise2014@gmail.com', 'hacker2014')
-print "done"
-
-
-DATE_FORMAT = "%Y-%m-%d"
-
-
-def daterange(start_date, end_date):
-  for n in range(int((end_date - start_date).days)):
-    yield start_date + timedelta(n)
-
-
-def load_bookings():
-  # Open a worksheet from spreadsheet with one shot
-  wks = gc.open("Bookings").sheet1
-  bookings_by_room = defaultdict(list)
-
-  for name, checkin_date, checkout_date, room_id in wks.get_all_values()[1:]:
-    bookings_by_room[room_id].append({
-      "name": name,
-      "checkin_date": datetime.strptime(checkin_date, DATE_FORMAT),
-      "checkout_date": datetime.strptime(checkout_date, DATE_FORMAT),
-      "room_id": room_id
-    })
-
-  # { "double 2": [12/01/2014, 13/01/2014...]} (datetime objects)
-  dates_occupied_by_room = defaultdict(set)
-  for room_id, bookings in bookings_by_room.iteritems():
-    for booking in bookings:
-      # TODO(AMK) error handling for double-booking
-      for booked_date in daterange(booking["checkin_date"],
-                                   booking["checkout_date"]):
-        dates_occupied_by_room[room_id].add(booked_date)
-
-  return dates_occupied_by_room
+bookings_db = BookingsDB()
 
 
 @app.route('/')
@@ -51,32 +15,27 @@ def calendar():
   min_checkin_date = datetime(2014, 9, 1)
   max_checkin_date = datetime(2014, 12, 1)
   calendar_date_range = list(daterange(min_checkin_date, max_checkin_date))
-  dates_occupied_by_room = load_bookings()
+  dates_occupied_by_room = bookings_db.dates_occupied_by_room
   rooms = dates_occupied_by_room.keys()
 
   return render_template("calendar.html", **locals())
 
 
-def room_available_during_range(room, start_date, end_date, dates_occupied_by_room):
-  return all(date not in dates_occupied_by_room[room]
-             for date in daterange(start_date, end_date))
-
-
 @app.route('/search')
 def search():
-  dates_occupied_by_room = load_bookings()
+  dates_occupied_by_room = bookings_db.dates_occupied_by_room
   rooms = dates_occupied_by_room.keys()
   if 'start_date' in request.args:
     start_date = parse(request.args['start_date'])
     end_date = parse(request.args['end_date'])
     available_rooms = [room for room in rooms
-      if room_available_during_range(room, start_date, end_date,
-                                     dates_occupied_by_room)
+      if bookings_db.room_available_during_range(room, start_date, end_date)
     ]
 
     return render_template('search_results.html', available_rooms=available_rooms)
   else:
     return render_template('search.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
