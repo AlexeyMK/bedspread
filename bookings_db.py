f70502c860c043f697a2084426e40c5583bf28aa
Alexey Komissarouk <alexey@alexeymk.com>
2014-09-20 13:50:25 -0600

    room properties available in DB for the querying

--- bookings_db.py
+++ bookings_db.py
@@ -18,21 +18,30 @@ class BookingsDB(object):
     self.bookings_by_room = defaultdict(list)
     # { "double 2": [12/01/2014, 13/01/2014...]} (datetime objects)
     self.dates_occupied_by_room = defaultdict(set)
+    self.room_properties = {}
 
     self.gc = gspread.login('hackerparadise2014@gmail.com', 'hacker2014')
+    self.spreadsheet = self.gc.open("Bookings")
     self._load_bookings()
+    self._load_rooms()
+
+  def _load_rooms(self):
+    rooms = self.spreadsheet.worksheet("Rooms")
+    room_cells = rooms.get_all_values()
+    room_property_names = room_cells[0]
+    for room_details_raw in room_cells[1:]:
+      self.room_properties[room_details_raw[0]] = \
+          dict(zip(room_property_names, room_details_raw))
 
   def _load_bookings(self):
-    # Open a worksheet from spreadsheet with one shot
-    spreadsheet = self.gc.open("Bookings")
-    bookings = spreadsheet.worksheet("Bookings")
+    bookings = self.spreadsheet.worksheet("Bookings")
 
     for room_id, name, checkin_date, checkout_date in bookings.get_all_values()[1:]:
       self.bookings_by_room[room_id].append({
         "name": name,
         "checkin_date": datetime.strptime(checkin_date, DATE_FORMAT),
         "checkout_date": datetime.strptime(checkout_date, DATE_FORMAT),
-        "room_id": room_id
+        "room_name": room_id
       })
 
     for room_id, bookings in self.bookings_by_room.iteritems():
@@ -43,7 +52,7 @@ class BookingsDB(object):
           self.dates_occupied_by_room[room_id].add(booked_date)
 
   def room_details(self, room_id):
-    return dict(name=room_id, category="single")
+    return self.room_properties[room_id]
 
   def room_available_during_range(self, room, start_date, end_date):
     return all(date not in self.dates_occupied_by_room[room]
